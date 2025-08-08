9ea730da1fccc0ac7de68a95ac9bc6a51327523f
Shoaib Sayyed <sayyedshoaib57@gmail.com>
2025-08-08 19:51:04 +0530

    chore: configured tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "./styles/global.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Hello, World!</h1>
+    <h1 className="text-red-500">Hello, World!</h1>
   </StrictMode>
 );
